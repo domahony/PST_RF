c96df3ad22a97a55afb119bbdeaec474d647f962
David O'Mahony <domahony@compusult.net>
2015-01-04 20:07:34 -0330

    Initial version.

--- PTS_RF.ino
+++ PTS_RF.ino
@@ -1,7 +1,3 @@
-/*
-The sketch is an empty template for Bluetooth Low Energy 4.
-Simply remove what you dont need, and fill in the rest.
-*/
 
 /*
  Copyright (c) 2014 OpenSourceRF.com.  All right reserved.
@@ -31,18 +27,16 @@ Simply remove what you dont need, and fill in the rest.
 
 #include <RFduinoBLE.h>
 
-int redPin = 2;
-int greenPin = 4;
+int lampPin = 2;
 
 void setup()
 {
-  RFduinoBLE.deviceName = "AAAAAA";
-  RFduinoBLE.advertisementData = "BBBBB";
+  RFduinoBLE.deviceName = "Lamp";
+  RFduinoBLE.advertisementData = "Office";
   RFduinoBLE.advertisementInterval = MILLISECONDS(300);
   RFduinoBLE.txPowerLevel = -20;  // (-20dbM to +4 dBm)
 
-  pinMode(redPin, OUTPUT);
-  pinMode(greenPin, OUTPUT);
+  pinMode(lampPin, OUTPUT);
   // start the BLE stack
   RFduinoBLE.begin();
 
@@ -55,11 +49,6 @@ void loop()
   // switch to lower power mode
   RFduino_ULPDelay(INFINITE);  
 
-  // to send one char
-  // RFduinoBLE.send((char)temp);
-
-  // to send multiple chars
-  // RFduinBLE.send(&data, len);
 }
 
 void RFduinoBLE_onAdvertisement(bool start)
@@ -83,23 +72,10 @@ void RFduinoBLE_onDisconnect()
 // returns the dBm signal strength indicated by the receiver after connection (-0dBm to -127dBm)
 void RFduinoBLE_onRSSI(int rssi)
 {
-  //Serial.print(rssi); Serial.println(" RSSI");
 }
 
 void RFduinoBLE_onReceive(char *data, int len)
 {
-
-  
-  if (data[0] == redPin || data[0] == greenPin) {
-    Serial.println("Writing Pin Data");
-   digitalWrite(data[0], data[1]);
- } 
- 
-
-  
-  Serial.println("On Recieve!!");
-  for (int i = 0; i < len; i++) {
-    Serial.print(data[i]);
-  }
-  Serial.println();
+  digitalWrite(lampPin, data[0]); 
+  Serial.print("Writing Pin Data"); Serial.println(data[0]);
 }
